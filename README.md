9459177ea4b7c3bf1b101d8585a00962619a3a4f
devidw <68775926+devidw@users.noreply.github.com>
2024-10-29 16:06:24 -0700

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+```bash
+deno task dev
+deno task build
+deno task preview
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let count = $state(1)
+</script>
+
+<button
+  onclick={() => {
+    count++
+  }}>need hug {"🤗".repeat(count)}</button
+>
